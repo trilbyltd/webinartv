26a7b315bff239b6440ea0715fe4f393a737658f
Dom Barnes <dom@dombarnes.com>
2016-05-16 16:44:05 +0100

    Enabling redis config in staging env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-if Rails.env == 'production'
+if Rails.env == ['production', 'staging']
   uri = URI.parse(ENV["REDIS_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
