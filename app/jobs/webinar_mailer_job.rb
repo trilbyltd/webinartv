d5d5dd46f0f628552ddb1d7517df67694833763e
Dom Barnes <dom@dombarnes.com>
2016-05-23 16:14:46 +0100

    Changing mailer to deliver_later

--- app/jobs/webinar_mailer_job.rb
+++ app/jobs/webinar_mailer_job.rb
@@ -2,7 +2,7 @@ class WebinarMailerJob < ActiveJob::Base
   queue_as :mailer
 
   def perform(attendee, webinar)
-    WebinarMailer.webinar_registration(attendee, webinar).deliver_now
-    WebinarMailer.internal_join_notification(attendee, webinar).deliver_now
+    WebinarMailer.webinar_registration(attendee, webinar).deliver_later
+    WebinarMailer.internal_join_notification(attendee, webinar).deliver_later
   end
 end
