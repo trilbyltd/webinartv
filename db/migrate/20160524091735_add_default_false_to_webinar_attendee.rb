26b16f874232e0abb96d34d4884301a7237fb418
Dom Barnes <dom@dombarnes.com>
2016-05-24 10:22:49 +0100

    Adding false default on attending

---
+++ db/migrate/20160524091735_add_default_false_to_webinar_attendee.rb
@@ -0,0 +1,6 @@
+class AddDefaultFalseToWebinarAttendee < ActiveRecord::Migration
+  def change
+    change_column_null :webinar_attendees, :attended, false
+    change_column_default :webinar_attendees, :attended, to: false
+  end
+end
